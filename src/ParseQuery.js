acf6d188233ae8f008a0d8cf63851071f8ec3b1d
Peter Shin <peterjs@fb.com>
2016-03-20 21:23:17 -0700

    Updating Parse.LiveQuery to use currentUserAsync.

--- src/ParseQuery.js
+++ src/ParseQuery.js
@@ -990,7 +990,7 @@ export default class ParseQuery {
   /**
    * Subscribe this query to get liveQuery updates
    * @method subscribe
-   * @return {LiveQuerySubscription} Returns the liveQuerySubscription, it's an event emitter
+   * @return {Parse.Promise} A promise that is resolved with a liveQuerySubscription
    * which can be used to get liveQuery updates.
    */
   subscribe(): any {
