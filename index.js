e718da647e80ec6ecc2929b2e9652fccd1f15615
Tyler Brock <tyler.brock@gmail.com>
2016-05-08 16:25:07 -0700

    Better check for react-native

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Export the correct version of the SDK for each platform that requires 'parse'
 if (typeof window === 'undefined') {
   module.exports = require('./lib/node/Parse.js');
-} else if (navigator && navigator.product === 'ReactNative') {
+} else if (typeof navigator === 'object' && navigator.product === 'ReactNative') {
   module.exports = require('./lib/react-native/Parse.js');
 } else {
   module.exports = require('./lib/browser/Parse.js');
