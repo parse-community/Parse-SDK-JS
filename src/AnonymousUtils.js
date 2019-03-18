a89e87b3eebf094f002ec21ffd4fa0f680b38802
Diamond Lewis <findlewis@gmail.com>
2019-03-18 13:15:23 -0500

    Improve documentation

--- src/AnonymousUtils.js
+++ src/AnonymousUtils.js
@@ -30,21 +30,29 @@ const authenticationProvider = {
 };
 
 /**
- * Provides utility functions for working with Anonymously logged-in users.
+ * Provides utility functions for working with Anonymously logged-in users. <br />
  * Anonymous users have some unique characteristics:
-    - Anonymous users don't need a user name or password.
-    - Once logged out, an anonymous user cannot be recovered.
-    - When the current user is anonymous, the following methods can be used to switch
-    to a different user or convert the anonymous user into a regular one:
-    - signUp converts an anonymous user to a standard user with the given username and password.
-    Data associated with the anonymous user is retained.
-    - logIn switches users without converting the anonymous user.
-    Data associated with the anonymous user will be lost.
-    - Service logIn (e.g. Facebook, Twitter) will attempt to convert
-    the anonymous user into a standard user by linking it to the service.
-    If a user already exists that is linked to the service, it will instead switch to the existing user.
-    - Service linking (e.g. Facebook, Twitter) will convert the anonymous user
-    into a standard user by linking it to the service.
+ * <ul>
+ *  <li>Anonymous users don't need a user name or password.</li>
+ *  <ul>
+ *    <li>Once logged out, an anonymous user cannot be recovered.</li>
+ *  </ul>
+ *  <li>signUp converts an anonymous user to a standard user with the given username and password.</li>
+ *  <ul>
+ *    <li>Data associated with the anonymous user is retained.</li>
+ *  </ul>
+ *  <li>logIn switches users without converting the anonymous user.</li>
+ *  <ul>
+ *    <li>Data associated with the anonymous user will be lost.</li>
+ *  </ul>
+ *  <li>Service logIn (e.g. Facebook, Twitter) will attempt to convert
+ *  the anonymous user into a standard user by linking it to the service.</li>
+ *  <ul>
+ *    <li>If a user already exists that is linked to the service, it will instead switch to the existing user.</li>
+ *  </ul>
+ *  <li>Service linking (e.g. Facebook, Twitter) will convert the anonymous user
+ *  into a standard user by linking it to the service.</li>
+ * </ul>
  * @class Parse.AnonymousUtils
  * @static
  */
