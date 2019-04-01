7f04642be96e22207527c887c30502e2f0a92a30
Diamond Lewis <findlewis@gmail.com>
2019-04-01 17:11:56 -0500

    Fix #775
    Prevent modules from compiling in react-native

--- src/ParseFile.js
+++ src/ParseFile.js
@@ -317,25 +317,27 @@ const DefaultController = {
     if (XHR) {
       return this.downloadAjax(uri);
     }
-    if (process.env.PARSE_BUILD === 'browser') {
+    if (process.env.PARSE_BUILD === 'browser' && !XHR) {
       return Promise.reject('Cannot make a request: No definition of XMLHttpRequest was found.');
     }
-    return new Promise((resolve, reject) => {
-      const client = uri.indexOf('https') === 0
-        ? require('https')
-        : require('http');
-      client.get(uri, (resp) => {
-        resp.setEncoding('base64');
-        let base64 = '';
-        resp.on('data', (data) => base64 += data);
-        resp.on('end', () => {
-          resolve({
-            base64,
-            contentType: resp.headers['content-type'],
+    if (process.env.PARSE_BUILD === 'node') {
+      return new Promise((resolve, reject) => {
+        const client = uri.indexOf('https') === 0
+          ? require('https')
+          : require('http');
+        client.get(uri, (resp) => {
+          resp.setEncoding('base64');
+          let base64 = '';
+          resp.on('data', (data) => base64 += data);
+          resp.on('end', () => {
+            resolve({
+              base64,
+              contentType: resp.headers['content-type'],
+            });
           });
-        });
-      }).on('error', reject);
-    });
+        }).on('error', reject);
+      });
+    }
   },
 
   downloadAjax: function(uri) {
