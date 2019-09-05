2fc8da088b53a4bfde36bb0b6cfab8aebc7b868e
Manuel <trezza.m@gmail.com>
2019-09-06 00:48:15 +0200

    Add support to get parse config with master key (#907)
    * added support for options in Parse.Config.get
    
    * added integration tests for parse config
    
    * Point parse-server to https://github.com/parse-community/parse-server/commit/ac40bf66f7cdeeb578fd716a8e3dd540470209ca
    
    * Fix docs

---
+++ integration/test/ParseConfigTest.js
@@ -0,0 +1,64 @@
+'use strict';
+
+const assert = require('assert');
+const clear = require('./clear');
+const Parse = require('../../node');
+
+function testConfig() {
+  const data = {
+    params: { internal: 'i', public: 'p' },
+    masterKeyOnly: { internal: true },
+  };
+  return Parse.CoreManager.getRESTController().request(
+    'PUT',
+    'config',
+    data,
+    { useMasterKey: true }
+  );
+}
+
+describe('Parse Config', () => {
+  beforeEach((done) => {
+    Parse.initialize('integration', null, 'notsosecret');
+    Parse.CoreManager.set('SERVER_URL', 'http://localhost:1337/parse');
+    Parse.Storage._clear();
+    clear().then(() => {
+      done();
+    });
+  });
+
+  it('can create a config', async () => {
+    const config = await Parse.Config.save({
+      str: 'hello',
+      num: 42
+    });
+    assert.equal(config.get('str'), 'hello');
+    assert.equal(config.get('num'), 42);
+  });
+
+  it('can get a config', async () => {
+    await Parse.Config.save({
+      str: 'hello',
+      num: 42
+    });
+    const config = await Parse.Config.get();
+    assert.equal(config.get('str'), 'hello');
+    assert.equal(config.get('num'), 42);
+  });
+
+  it('can get internal config parameter with masterkey', async () => {
+    await testConfig();
+
+    const config = await Parse.Config.get({ useMasterKey: true });
+    assert.equal(config.get('internal'), 'i');
+    assert.equal(config.get('public'), 'p');
+  });
+
+  it('cannot get internal config parameter without masterkey', async () => {
+    await testConfig();
+
+    const config = await Parse.Config.get();
+    assert.equal(config.get('internal'), undefined);
+    assert.equal(config.get('public'), 'p');
+  });
+});
