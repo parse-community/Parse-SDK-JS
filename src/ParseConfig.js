8b2afadf8709fedb46dcaf65d2c00abac6fa15ca
Manuel Trezza <trezza.m@gmail.com>
2019-09-06 02:06:33 +0200

    added docs; removed unnecessary encoding

--- src/ParseConfig.js
+++ src/ParseConfig.js
@@ -92,14 +92,20 @@ class ParseConfig {
   /**
    * Save value keys to the server.
    * @static
+   * @param {Object} attrs The config parameters and values.
+   * @param {Object} masterKeyOnlyFlags The flags that define whether config parameters listed
+   * in `attrs` should be retrievable only by using the master key.
+   * For example: `param1: true` makes `param1` only retrievable by using the master key.
+   * If a parameter is not provided or set to `false`, it can be retrieved without
+   * using the master key.
    * @return {Promise} A promise that is resolved with a newly-created
    *     configuration object or with the current with the update.
    */
-  static save(attrs: { [key: string]: any }, masterKeyOnly: { [key: string]: any }) {
+  static save(attrs: { [key: string]: any }, masterKeyOnlyFlags: { [key: string]: any }) {
     const controller = CoreManager.getConfigController();
     //To avoid a mismatch with the local and the cloud config we get a new version
-    return controller.save(attrs, masterKeyOnly).then(() => {
-      return controller.get();
+    return controller.save(attrs, masterKeyOnlyFlags).then(() => {
+      return controller.get({useMasterKey: true});
     },(error) => {
       return Promise.reject(error);
     });
@@ -184,20 +190,16 @@ const DefaultController = {
     });
   },
 
-  save(attrs: { [key: string]: any }, masterKeyOnly: { [key: string]: any }) {
+  save(attrs: { [key: string]: any }, masterKeyOnlyFlags: { [key: string]: any }) {
     const RESTController = CoreManager.getRESTController();
     const encodedAttrs = {};
-    const encodedMasterKeyOnly = {};
     for(const key in attrs){
       encodedAttrs[key] = encode(attrs[key])
-    }
-    for(const key in masterKeyOnly){
-      encodedMasterKeyOnly[key] = encode(masterKeyOnly[key])
     }
     return RESTController.request(
       'PUT',
       'config',
-      { params: encodedAttrs, masterKeyOnly: encodedMasterKeyOnly },
+      { params: encodedAttrs, masterKeyOnly: masterKeyOnlyFlags },
       { useMasterKey: true }
     ).then(response => {
       if(response && response.result){
