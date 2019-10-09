0650259d7975ce40d16b4d723b03049a7fac8da9
Diamond Lewis <findlewis@gmail.com>
2019-10-09 14:28:14 -0500

    clean up

--- build_releases.js
+++ build_releases.js
@@ -18,6 +18,10 @@ const rmDir = function(dirPath) {
   }
 };
 
+const exec = function(cmd) {
+  execSync(cmd, { stdio: 'inherit' });
+};
+
 console.log(`Building JavaScript SDK v${pkg.version}...\n`)
 
 console.log('Cleaning up old builds...\n');
@@ -29,19 +33,19 @@ const crossEnv = 'npm run cross-env';
 const gulp = 'npm run gulp';
 
 console.log('Browser Release:');
-execSync(`${crossEnv} PARSE_BUILD=browser ${gulp} compile`, { stdio: 'inherit' });
+exec(`${crossEnv} PARSE_BUILD=browser ${gulp} compile`);
 
 console.log('Weapp Release:');
-execSync(`${crossEnv} PARSE_BUILD=weapp ${gulp} compile`, { stdio: 'inherit' });
+exec(`${crossEnv} PARSE_BUILD=weapp ${gulp} compile`);
 
 console.log('Node.js Release:');
-execSync(`${crossEnv} PARSE_BUILD=node ${gulp} compile`, { stdio: 'inherit' });
+exec(`${crossEnv} PARSE_BUILD=node ${gulp} compile`);
 
 console.log('React Native Release:');
-execSync(`${crossEnv} PARSE_BUILD=react-native ${gulp} compile`, { stdio: 'inherit' });
+exec(`${crossEnv} PARSE_BUILD=react-native ${gulp} compile`);
 
 console.log('Bundling and minifying for CDN distribution:');
-execSync(`${gulp} browserify`, { stdio: 'inherit' });
-execSync(`${gulp} browserify-weapp`, { stdio: 'inherit' });
-execSync(`${gulp} minify`, { stdio: 'inherit' });
-execSync(`${gulp} minify-weapp`, { stdio: 'inherit' });
+exec(`${gulp} browserify`);
+exec(`${gulp} browserify-weapp`);
+exec(`${gulp} minify`);
+exec(`${gulp} minify-weapp`);
