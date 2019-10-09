b9eb60321daced96afb690367bf627ca25b46c2c
Diamond Lewis <findlewis@gmail.com>
2019-10-09 11:01:56 -0500

    Support compiling on Windows
    Converts bash script to node script.
    
    No longer requires gulp to be installed globally.
    
    Fixes: https://github.com/parse-community/Parse-SDK-JS/issues/925
    
    @JeromeDeLeon Can you test if this works?

---
+++ build_releases.js
@@ -0,0 +1,47 @@
+const pkg = require('./package.json');
+const fs = require('fs');
+const path = require('path');
+const { execSync } = require('child_process');
+
+const rmDir = function(dirPath) {
+  if(fs.existsSync(dirPath)) {
+    const files = fs.readdirSync(dirPath);
+    files.forEach(function(file) {
+      const curPath = path.join(dirPath, file);
+      if(fs.lstatSync(curPath).isDirectory()) { 
+        rmDir(curPath);
+      } else {
+        fs.unlinkSync(curPath);
+      }
+    });
+    fs.rmdirSync(dirPath);
+  }
+};
+
+console.log(`Building JavaScript SDK v${pkg.version}...\n`)
+
+console.log('Cleaning up old builds...\n');
+
+rmDir(path.join(__dirname, 'dist'));
+rmDir(path.join(__dirname, 'lib'));
+
+const crossEnv = path.join(__dirname, 'node_modules', 'cross-env', 'dist', 'bin', 'cross-env.js');
+const gulp = path.join(__dirname, 'node_modules', 'gulp', 'bin', 'gulp.js');
+
+console.log('Browser Release:');
+execSync(`${crossEnv} PARSE_BUILD=browser ${gulp} compile`, { stdio: 'inherit' });
+
+console.log('Weapp Release:');
+execSync(`${crossEnv} PARSE_BUILD=weapp ${gulp} compile`, { stdio: 'inherit' });
+
+console.log('Node.js Release:');
+execSync(`${crossEnv} PARSE_BUILD=node ${gulp} compile`, { stdio: 'inherit' });
+
+console.log('React Native Release:');
+execSync(`${crossEnv} PARSE_BUILD=react-native ${gulp} compile`, { stdio: 'inherit' });
+
+console.log('Bundling and minifying for CDN distribution:');
+execSync(`${gulp} browserify`, { stdio: 'inherit' });
+execSync(`${gulp} browserify-weapp`, { stdio: 'inherit' });
+execSync(`${gulp} minify`, { stdio: 'inherit' });
+execSync(`${gulp} minify-weapp`, { stdio: 'inherit' });
