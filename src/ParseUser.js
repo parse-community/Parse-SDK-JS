8327a55c7510f9d3b456a4439237e6274b6e564a
Diamond Lewis <findlewis@gmail.com>
2019-11-14 13:35:17 -0600

    Fix #992
    Revert https://github.com/parse-community/Parse-SDK-JS/pull/978

--- src/ParseUser.js
+++ src/ParseUser.js
@@ -869,7 +869,7 @@ const DefaultController = {
   updateUserOnDisk(user) {
     const path = Storage.generatePath(CURRENT_USER_KEY);
     const json = user.toJSON();
-    json.className = user.constructor.name === 'ParseUser' ? '_User' : user.constructor.name;
+    json.className = '_User';
     return Storage.setItemAsync(
       path, JSON.stringify(json)
     ).then(() => {
