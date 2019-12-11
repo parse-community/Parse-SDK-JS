41cb82a19d3dcae7564ef7ba99701e12c4c7b309
Diamond Lewis <findlewis@gmail.com>
2019-12-10 18:40:59 -0600

    Support query.cancel() on Node
    Moves abort event to prevent xhr.open from calling it.
    Ensure requestTask is set after the request sends.

--- src/__tests__/ParseQuery-test.js
+++ src/__tests__/ParseQuery-test.js
@@ -2124,7 +2124,7 @@ describe('ParseQuery', () => {
 
     await query.find();
 
-    expect(query._xhrRequest.task).toEqual(mockRequestTask);
+    expect(query._xhrRequest.task).toEqual(null);
     query.cancel();
     expect(mockRequestTask.abort).toHaveBeenCalledTimes(1);
   });
