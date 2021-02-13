3494fde0fba3bc39959cb6e1a456d7366c50ad84
Diamond Lewis <findlewis@gmail.com>
2021-02-12 19:08:39 -0600

    Add indexedDB check

--- src/IndexedDBStorageController.js
+++ src/IndexedDBStorageController.js
@@ -4,25 +4,29 @@
 
 import { createStore, del, set, get, clear, keys } from 'idb-keyval';
 
-const ParseStore = createStore('parseDB', 'parseStore');
+try {
+  const ParseStore = createStore('parseDB', 'parseStore');
 
-export const IndexedDBStorageController = {
-  async: 1,
-  getItemAsync(path: string) {
-    return get(path, ParseStore);
-  },
-  setItemAsync(path: string, value: string) {
-    return set(path, value, ParseStore);
-  },
-  removeItemAsync(path: string) {
-    return del(path, ParseStore);
-  },
-  getAllKeysAsync() {
-    return keys(ParseStore);
-  },
-  clear() {
-    return clear(ParseStore);
-  },
-};
+  const IndexedDBStorageController = {
+    async: 1,
+    getItemAsync(path: string) {
+      return get(path, ParseStore);
+    },
+    setItemAsync(path: string, value: string) {
+      return set(path, value, ParseStore);
+    },
+    removeItemAsync(path: string) {
+      return del(path, ParseStore);
+    },
+    getAllKeysAsync() {
+      return keys(ParseStore);
+    },
+    clear() {
+      return clear(ParseStore);
+    },
+  };
 
-module.exports = IndexedDBStorageController;
+  module.exports = IndexedDBStorageController;
+} catch (e) {
+  // IndexedDB not supported
+}
