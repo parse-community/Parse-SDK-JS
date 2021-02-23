dfd1e226d8e5a64a94995e8001041389eea9c552
Diamond Lewis <findlewis@gmail.com>
2021-02-23 05:41:37 -0600

    remove ignore code

--- src/ParseObject.js
+++ src/ParseObject.js
@@ -741,15 +741,6 @@ class ParseObject {
 
     const currentAttributes = this.attributes;
 
-    // Only set nested fields if exists
-    const serverData = this._getServerData();
-    if (typeof key === 'string' && key.includes('.')) {
-      const field = key.split('.')[0];
-      if (!serverData[field]) {
-        return this;
-      }
-    }
-
     // Calculate new values
     const newValues = {};
     for (const attr in newOps) {
