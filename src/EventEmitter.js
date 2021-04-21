7512f533026431f7a085fb3f88d4b5c30430877c
Duy Bao Nguyen <bduyng@gmail.com>
2021-04-21 11:18:55 +0700

    fix EventEmitter undefined on React Native 0.64 (#1351)
    * fix EventEmitter undefined on React Native 0.64
    
    * fix bug on react-native-test.js

--- src/EventEmitter.js
+++ src/EventEmitter.js
@@ -10,7 +10,7 @@
  */
 
 if (process.env.PARSE_BUILD === 'react-native') {
-  const EventEmitter = require('../../../react-native/Libraries/vendor/emitter/EventEmitter');
+  const EventEmitter = require('../../../react-native/Libraries/vendor/emitter/_EventEmitter');
   EventEmitter.prototype.on = EventEmitter.prototype.addListener;
   module.exports = EventEmitter;
 } else {
