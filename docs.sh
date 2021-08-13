eb5a83ee53b253ed10806f3d1bec76e72fb7d39a
dblythy <daniel-blyth@live.com.au>
2021-08-13 18:43:03 +1000

    remove sh

--- docs.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh -e
-find ./ -type f -exec sed -i '' 's#@generic#@ignore generic#' *.js {} \;
-jsdoc -c ./jsdoc-conf.json ./src
-find ./ -type f -exec sed -i '' 's#@ignore generic#@generic#' *.js {} \;
