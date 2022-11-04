5e6328ccaa354e1c7ccdd113eaf2dcf6730af484
dblythy <daniel-blyth@live.com.au>
2022-11-04 14:36:03 +1100

    Update ParseObject.js

--- src/ParseObject.js
+++ src/ParseObject.js
@@ -118,7 +118,7 @@ const nestedHandler = {
     const prop = target[key];
     if (
       Object.prototype.toString.call(prop) === '[object Object]' &&
-      !(prop instanceof ParseObject)
+      !prop?.constructor?.name?.includes('Parse')
     ) {
       const thisHandler = { ...nestedHandler };
       thisHandler._path = `${this._path}.${key}`;
@@ -148,7 +148,7 @@ const proxyHandler = {
     const getValue = receiver.get(key);
     if (
       Object.prototype.toString.call(getValue) === '[object Object]' &&
-      !(getValue instanceof ParseObject)
+      !getValue?.constructor?.name?.includes('Parse')
     ) {
       const thisHandler = { ...nestedHandler };
       thisHandler._path = key;
