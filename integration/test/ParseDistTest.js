182e24c90e47346d7c0e831e389fa6bc2ed8e43e
Diamond Lewis <findlewis@gmail.com>
2023-01-26 20:14:43 -0600

    check for open connections after tests have finished

--- integration/test/ParseDistTest.js
+++ integration/test/ParseDistTest.js
@@ -1,12 +1,19 @@
 const puppeteer = require('puppeteer');
+let browser = null;
 let page = null;
 for (const fileName of ['parse.js', 'parse.min.js']) {
-  beforeAll(async () => {
-    const browser = await puppeteer.launch();
-    page = await browser.newPage();
-    await page.goto(`http://localhost:1337/${fileName}`);
-  });
   describe(`Parse Dist Test ${fileName}`, () => {
+    beforeEach(async () => {
+      browser = await puppeteer.launch();
+      page = await browser.newPage();
+      await page.goto(`http://localhost:1337/${fileName}`);
+    });
+
+    afterEach(async () => {
+      await page.close();
+      await browser.close();
+    });
+
     it('can save an object', async () => {
       const response = await page.evaluate(async () => {
         const object = await new Parse.Object('TestObject').save();
@@ -17,6 +24,7 @@ for (const fileName of ['parse.js', 'parse.min.js']) {
       expect(obj).toBeDefined();
       expect(obj.id).toEqual(response);
     });
+
     it('can query an object', async () => {
       const obj = await new Parse.Object('TestObject').save();
       const response = await page.evaluate(async () => {
