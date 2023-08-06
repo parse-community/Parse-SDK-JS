41a41b420ab5b464904a80be30ebac058e494f12
Diamond Lewis <findlewis@gmail.com>
2023-08-06 16:01:45 -0500

    remove extra comments

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,5 +1,4 @@
 // https://github.com/DefinitelyTyped/DefinitelyTyped/blob/b23a36e669fa127d1035e22ca93faab85b98e49f/types/parse/index.d.ts#L11
-// TODO: Possibly add @types/node module reference types
 
 /// <reference path="node.d.ts" />
 /// <reference path="react-native.d.ts" />
