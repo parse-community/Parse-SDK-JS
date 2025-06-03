90c6b69f7d0dea194bd066ab8e7e8b539005ae56
Manuel Trezza <5673677+mtrezza@users.noreply.github.com>
2025-06-03 22:33:04 +0200

    fix

--- integration/test/IdempotencyTest.js
+++ integration/test/IdempotencyTest.js
@@ -34,9 +34,8 @@ describe('Idempotency', () => {
 
   it('handle duplicate job request', async () => {
     DuplicateRequestId('1234');
-    const params = { startedBy: 'Monty Python' };
-    const jobStatusId = await Parse.Cloud.startJob('CloudJob1', params);
-    await expectAsync(Parse.Cloud.startJob('CloudJob1', params)).toBeRejectedWithError(
+    const jobStatusId = await Parse.Cloud.startJob('CloudJob1', {});
+    await expectAsync(Parse.Cloud.startJob('CloudJob1', {})).toBeRejectedWithError(
       'Duplicate request'
     );
 
@@ -49,7 +48,6 @@ describe('Idempotency', () => {
     }
     const jobStatus = await Parse.Cloud.getJobStatus(jobStatusId);
     expect(jobStatus.get('status')).toBe('succeeded');
-    expect(jobStatus.get('params').startedBy).toBe('Monty Python');
   });
 
   it('handle duplicate POST / PUT request', async () => {
--- integration/test/ParseCloudTest.js
+++ integration/test/ParseCloudTest.js
@@ -94,14 +94,12 @@ describe('Parse Cloud', () => {
   });
 
   it('run job', async () => {
-    const params = { startedBy: 'Monty Python' };
-    const jobStatusId = await Parse.Cloud.startJob('CloudJob1', params);
+    const jobStatusId = await Parse.Cloud.startJob('CloudJob1', {});
     expect(jobStatusId).toBeDefined();
     await waitForJobStatus(jobStatusId, 'succeeded');
 
     const jobStatus = await Parse.Cloud.getJobStatus(jobStatusId);
     assert.equal(jobStatus.get('status'), 'succeeded');
-    assert.equal(jobStatus.get('params').startedBy, 'Monty Python');
   });
 
   it('run long job', async () => {
