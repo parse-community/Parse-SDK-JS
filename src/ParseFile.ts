77e5e2c36a4cc521b19da13c28eedb2eadb93583
Manuel Trezza <5673677+mtrezza@users.noreply.github.com>
2026-02-25 20:57:23 +0000

    docs

--- src/ParseFile.ts
+++ src/ParseFile.ts
@@ -275,6 +275,7 @@ class ParseFile {
 
   /**
    * Gets the directory of the file.
+   * Requires Parse Server >= 9.4.0.
    *
    * @returns {string | undefined}
    */
@@ -520,6 +521,7 @@ class ParseFile {
   /**
    * Sets the directory where the file will be stored.
    * Requires the Master Key when saving.
+   * Requires Parse Server >= 9.4.0.
    *
    * @param {string} directory the directory path
    */
